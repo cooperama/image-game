7d250f2ea7f78a59fb23c4d3d250da7649ac1a19
bhowe111 <brendan.howe1@gmail.com>
2021-01-05 19:59:34 -0800

    Created card list page for testing

--- image-game/App.js
+++ image-game/App.js
@@ -2,15 +2,18 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Landing from "./screens/Landing";
+// import CardList from "./screens/CardList";
 
 export default function App() {
-  return <Landing />;
+  return (
+    <Landing />
+    // <CardList />
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
